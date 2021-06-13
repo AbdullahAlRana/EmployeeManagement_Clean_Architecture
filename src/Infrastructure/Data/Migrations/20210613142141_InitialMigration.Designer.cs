46ae2bc0517c35bb4a689fa312474ab40d54163a
Abdullah Rana <Abdullahalrana@gmail.com>
2021-06-13 20:25:05 +0600

    All Okay

--- src/Infrastructure/Data/Migrations/20210613142141_InitialMigration.Designer.cs
+++ src/Infrastructure/Data/Migrations/20210613142141_InitialMigration.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EmployeeManagement.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20210613070215_InitialMigration")]
+    [Migration("20210613142141_InitialMigration")]
     partial class InitialMigration
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
